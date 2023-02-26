95898cb397a24d47e7f7c1a8dc6badca84281024
clara0624 <alvin.clara.06@gmail.com>
2023-02-26 11:34:28 +0900

    자바

---
+++ src/lecture_20230226/Lecture_10.java
@@ -0,0 +1,11 @@
+package lecture_20230226;
+
+import java.util.Scanner;
+
+public class Lecture_10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+
+    }
+}
